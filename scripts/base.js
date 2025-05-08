64f62101927d95e30c02b8b4e79153c35e3eecc9
flixjavier <flixjavier@gmail.com>
2025-05-07 21:32:20 -0600

    Update

---
+++ scripts/base.js
@@ -0,0 +1,35 @@
+
+//Hamburger menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+  
+});
+
+// use the date object
+const today = new Date();
+
+const options = {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false // 24-hour format
+};
+
+const formattedDate = new Intl.DateTimeFormat('en-US', options).format(today);
+
+const year = document.querySelector("#current-year");
+
+const lastModified = document.querySelector("#lastModified");
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `<span class="highlight">Last modification: ${formattedDate}</span>`; 
+
+
