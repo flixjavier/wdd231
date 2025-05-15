e6e1a6baa27de7bef0557ac1cf7d9c6ef1c158b0
flixjavier <flixjavier@gmail.com>
2025-05-14 21:50:36 -0600

    delete some unused lines

--- scripts/base.js
+++ scripts/base.js
@@ -165,14 +165,3 @@ function coursesCardsTemplate(array) {
         cardsTemplate.appendChild(cardTemplate);
     });
 };
-
-/* subject: 'CSE',
-        number: 110,
-        title: 'Introduction to Programming',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
-        technology: [
-            'Python'
-        ],
-        completed: true */
