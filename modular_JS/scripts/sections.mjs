7e82ab9db1095b1e92c7b67777ce3232f6044dd7
flixjavier <flixjavier@gmail.com>
2025-06-04 10:54:18 -0600

    Added Modules example

---
+++ modular_JS/scripts/sections.mjs
@@ -0,0 +1,13 @@
+
+
+function setSectionSelection(course) {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  course.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
+
+export default setSectionSelection;
