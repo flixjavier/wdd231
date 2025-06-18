a74ba18d9caf982da6643fcbbc36a71d0b4b9be5
flixjavier <flixjavier@gmail.com>
2025-06-18 12:18:22 -0600

    finish thank you html

--- project/scripts/thanks.js
+++ project/scripts/thanks.js
@@ -49,3 +49,61 @@ home.addEventListener("click", () => {
   window.location.href = 'index.html'
 });
 
+
+// use the date object
+const today = new Date();
+
+const options = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false // 24-hour format
+};
+
+const formattedDate = new Intl.DateTimeFormat('en-US', options).format(today);
+
+const year = document.querySelector("#current-year");
+
+const lastModified = document.querySelector("#lastModified");
+
+const footerInfo = document.querySelector(".footer-info");
+
+const socialIcons = document.querySelector(".social-icons");
+
+footerInfo.innerHTML = `<address>
+					<strong>AGROAVE Mexico</strong><br />
+					Rancho Nicaragua<br />
+					Camargo, Chihuahua<br />
+					aveprodcutos77@gmail.com<br />
+					(614-175-5234)
+				</address>`
+
+socialIcons.innerHTML = `<img src="images/instagram_logo.svg" alt="Instagram" id="instagram"/><img src="images/linkedin_logo.svg" alt="LinkedIn Logo" id="linkedin"/><img src="images/github_logo.svg" alt="GitHub Logo" id="github" />`
+
+document.getElementById('instagram').addEventListener('click', function() {
+  window.open('https://www.instagram.com/flixjavier/', '_blank');
+});
+
+document.getElementById('linkedin').addEventListener('click', function() {
+  window.open('https://www.linkedin.com/in/felixjflores-zamarripa/');
+});
+
+document.getElementById('github').addEventListener('click', function() {
+  window.open('https://github.com/flixjavier', '_blank');
+});
+	
+
+year.innerHTML = `<span class="highlight">&copy${today.getFullYear()} WDD231 Class Project |
+				Felix Flores<br /> Camargo, Chihuahua<br /></span> <img
+				src="images/onion.webp"
+				alt="onion image"
+				class="flag"
+				width="50"
+				height="50"
+			/>`;
+      /* can add loading="Lazy" */
+
+lastModified.innerHTML = `<span class="highlight">Last modification: ${formattedDate}</span>`; 
